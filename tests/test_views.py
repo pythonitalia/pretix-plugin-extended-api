3a2749bfef026b5a941548170ba845c2744894d2
Marco Acierno <marcoaciernoemail@gmail.com>
2022-01-08 16:01:57 +0100

    add not working unit-test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,3 +1,10 @@
-def test_view():
+import pytest
+pytestmark = pytest.mark.django_db
+
+
+def test_view(token_client, event):
     # put your first tests here
-    assert 1 + 1 == 2
+    # /api/v1/organizers/test/events/event/testapi/
+    # http://localhost:8000/api/v1/organizers/test/events/test-event/testapi/
+    resp = token_client.get('/api/v1/organizers/dummy/events/dummy/testapi/')
+    assert resp.status_code == 200

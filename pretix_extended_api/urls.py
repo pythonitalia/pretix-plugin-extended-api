b38a740e02dc3694e6f0f0d385ad24d864205fb8
Marco Acierno <marcoaciernoemail@gmail.com>
2022-02-12 02:06:08 +0100

    API to Get multiple Orders

--- pretix_extended_api/urls.py
+++ pretix_extended_api/urls.py
@@ -1,5 +1,7 @@
 from pretix.api.urls import event_router
 
 from .views.tickets import TicketsViewSet
+from .views.orders import OrdersViewSet
 
 event_router.register("tickets", TicketsViewSet, basename="tickets")
+event_router.register("extended-orders", OrdersViewSet, basename="extended-orders")
